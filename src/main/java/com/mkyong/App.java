f8652e853ad8967af9d289d6c6123fc28dad184d
deepak <dsriniv8@asu.edu>
2017-10-18 12:39:00 -0700

    Changes for dev

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -6,19 +6,20 @@ import java.util.UUID;
  * Generate a unique number
  *
  */
-public class App 
+public class App
 {
 
     public static void main( String[] args )
     {
         App obj = new App();
         System.out.println("Unique ID : " + obj.generateUniqueKey());
+        System.out.println("Dummy Change");
     }
-    
+
     public String generateUniqueKey(){
-    	
+
     	String id = UUID.randomUUID().toString();
     	return id;
-    	
+
     }
 }
